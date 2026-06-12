7ccee35a5ad63c19afc571b49cfc2ad6d84bfd5c
jaswanthsingala <jaswanthsingala@gmail.com>
2026-06-12 12:48:58 +0530

    Add files via upload

---
+++ ass2.py
@@ -0,0 +1,12 @@
+'''
+2️ Write a program to:
+Take one number and check whether it is:
+•	Even
+•	Or Odd
+
+'''
+num = int(input("enter a valid number: "))
+if(num%2 == 0):
+    print("even")
+else:
+    print("odd")
